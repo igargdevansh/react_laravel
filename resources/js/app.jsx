63a9095982e9304a5b17c7e9ffb19eddaab84f04
Devansh Garg <gargdevansh523@gmail.com>
2025-08-03 00:55:55 +0530

    added new feature

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div className="text-center mt-10 text-3xl text-blue-500">
-      Hello from React + Tailwind!
+    hii
     </div>
   )
 }
